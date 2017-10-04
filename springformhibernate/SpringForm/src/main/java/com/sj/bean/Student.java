ca23e57baa67609289d56c071b0b62ce8b3fdf88
jangachary sriramadas <jangacharysri@gmail.com>
2017-10-04 11:49:40 +0530

    Add files via upload
    Spring MVC application to read user inputs and stores them in Mysql database using hibernate.

---
+++ springformhibernate/SpringForm/src/main/java/com/sj/bean/Student.java
@@ -0,0 +1,51 @@
+package com.sj.bean;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+
+	public Student() {
+		System.out.println(getClass());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
